62164d38d6c889b7724ee5edda9c4811260d4330
Fernando Righi <43839098+whatsmynameagain@users.noreply.github.com>
2025-01-02 20:16:56 -0300

    add asteroid shooting logic

--- asteroid.py
+++ asteroid.py
@@ -1,5 +1,6 @@
-import pygame
+import pygame, random
 from circleshape import CircleShape
+from constants import *
 
 class Asteroid(CircleShape):
 
@@ -11,3 +12,15 @@ class Asteroid(CircleShape):
     
     def update(self, dt):
         self.position += (self.velocity * dt)
+
+    def split(self):
+        if self.radius > ASTEROID_MIN_RADIUS:
+            rand_angle = random.uniform(20, 50)
+            rand_angle_a = self.velocity.rotate(rand_angle)
+            rand_angle_b = self.velocity.rotate(-rand_angle)
+            new_asteroid_radius = self.radius - ASTEROID_MIN_RADIUS
+            asteroid_a = Asteroid(self.position.x, self.position.y, new_asteroid_radius)
+            asteroid_b = Asteroid(self.position.x, self.position.y, new_asteroid_radius)
+            asteroid_a.velocity = rand_angle_a * 1.2
+            asteroid_b.velocity = rand_angle_b * 1.2
+        self.kill()
--- main.py
+++ main.py
@@ -39,7 +39,7 @@ def main():
             for shot in shots:
                 if asteroid.check_collision(shot):
                     shot.kill()
-                    asteroid.kill()
+                    asteroid.split()
             if asteroid.check_collision(player):
                 print("Game over!")
                 sys.exit()
